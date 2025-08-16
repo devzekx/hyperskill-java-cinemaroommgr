2776564ee7dd823a482ef77774ca1d469fed5666
devzekx <dev.zekx@gmail.com>
2025-08-16 16:53:13 +0200

    wrestling through 2d arrays with input/output

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -8,12 +8,40 @@ public class Cinema {
 
         // vars
         String income = "Total income:";
+        String location = "Cinema:";
+
 
         // ask input here
         System.out.println("Enter the number of rows:");
         int rows = input.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int seats = input.nextInt();
+
+        String[] header = {" ", "1", "2", "3", "4", "5", "6", "7", "8"};
+        String[][] data = {
+                {"1", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"2", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"3", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"4", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"5", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"6", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"7", "S", "S", "S", "S", "S", "S", "S", "S"}
+        };
+
+        // print header method
+        for (int i = 0; i < rows; i++) {
+            System.out.print(header[i] + " ");
+        }
+
+        // print body method
+        for (int i = 0; i < data.length; i++) {
+            System.out.println();
+            for (int j = 0; j < data[i].length; j++){
+                System.out.print(data[i][j] + " ");
+            }
+        }
+
+
 
         // calculate logic here
         int totalSeats = seats * rows;
@@ -29,6 +57,7 @@ public class Cinema {
             int priceBackRows = backRows * seats * 8;
             int priceOdd = priceFrontRows + priceBackRows;
             System.out.println(income);
+
             System.out.println("$" + priceOdd);
         }
 
@@ -48,13 +77,11 @@ public class Cinema {
                 {"6", "S", "S", "S", "S", "S", "S", "S", "S"},
                 {"7", "S", "S", "S", "S", "S", "S", "S", "S"}
         };
-         */
 
                 /* print header method
         for (int i = 0; i < header.length; i++) {
             System.out.print(header[i] + " ");
         }
-        */
 
         /* print body method
         for (int i = 0; i < data.length; i++) {
