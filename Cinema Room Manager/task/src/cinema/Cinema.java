1b292dd2c7125aadb590144146257ce23bc3cc48
devzekx <dev.zekx@gmail.com>
2025-08-16 19:33:47 +0200

    figuring out 2D char arrays and adding to them.

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,4 +1,5 @@
 package cinema;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
@@ -17,30 +18,18 @@ public class Cinema {
         System.out.println("Enter the number of seats in each row:");
         int seats = input.nextInt();
 
-        String[] header = {" ", "1", "2", "3", "4", "5", "6", "7", "8"};
-        String[][] data = {
-                {"1", "S", "S", "S", "S", "S", "S", "S", "S"},
-                {"2", "S", "S", "S", "S", "S", "S", "S", "S"},
-                {"3", "S", "S", "S", "S", "S", "S", "S", "S"},
-                {"4", "S", "S", "S", "S", "S", "S", "S", "S"},
-                {"5", "S", "S", "S", "S", "S", "S", "S", "S"},
-                {"6", "S", "S", "S", "S", "S", "S", "S", "S"},
-                {"7", "S", "S", "S", "S", "S", "S", "S", "S"}
-        };
+        // create 2D array from input
 
-        // print header method
-        for (int i = 0; i < rows; i++) {
-            System.out.print(header[i] + " ");
-        }
+        char[][] data = new char[rows][seats];
 
-        // print body method
-        for (int i = 0; i < data.length; i++) {
-            System.out.println();
+        for (int i = 0; i < data.length; i++){
             for (int j = 0; j < data[i].length; j++){
-                System.out.print(data[i][j] + " ");
+                data[i][j]++;
+                System.out.print(Arrays.toString(data) + " ");
             }
         }
 
+        // print he
 
 
         // calculate logic here
